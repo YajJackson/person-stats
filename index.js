1aadb6993b7497913b7204f32fe1da926d90f9ad
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-06-27 10:37:31 -0400

    implemented template ` and appendChild

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 function updateContent(ev) {
-    ev.preventDefault() //ask a question about this Why is it neeed
-    const form = ev.target
-    const name = form.personName.value
-    const age = form.personAge.value
-    const color = form.personColor.value
+  ev.preventDefault() //ask a question about this Why is it neeed
+  
+  const form = ev.target
+  const name = form.personName.value
+  const age = form.personAge.value
+  const color = form.personColor.value
 
-    console.log(name, age, color)
-    updateStats(name, age, color)
+  updateStats(name, age, color)
 }
 
 function updateStats(name, age, color) {
-    const statsBox = document.querySelector('#personStats')
-    statsBox.innerHTML += name + " " + age+ "<br>"
-    document.querySelector('#statsContainer').style.color = color;
+  const statsBox = document.querySelector('#personStats')
+  const p = document.createElement('p')
+
+  p.textContent = `Name: ${name}, Age: ${age}, Color: ${color}`
+  p.style.color = color
+
+  statsBox.appendChild(p)
 }
 
 const personForm  = document.querySelector('#personForm')
